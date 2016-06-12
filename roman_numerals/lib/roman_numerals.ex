dd7f7f4633fc7a8d135a3dc1827d46532e9633ae
Stratos Xakoustos <stratouklos@gmail.com>
2016-06-12 11:42:39 -0400

    roman numerals done with only function pattern matching

--- roman_numerals/lib/roman_numerals.ex
+++ roman_numerals/lib/roman_numerals.ex
@@ -1,15 +1,55 @@
 defmodule RomanNumerals do
 
-    def to_roman(n) when n < 5 do
+    def to_roman(n) when n < 4 do
       String.duplicate("I", n)
     end
 
-    def to_roman(n) when n < 10 do
+    def to_roman(4) do
+      "IV"
+    end
+
+    def to_roman(n) when n < 9 do
       "V" <> to_roman(n - 5)
     end
 
-    def to_roman(n)  do
+    def to_roman(n) when n < 10 do
+      "IX" <> to_roman(n - 9)
+    end
+
+    def to_roman(n) when n < 40 do
       "X" <> to_roman(n - 10)
+    end
+
+    def to_roman(n) when n < 50 do
+      "XL" <> to_roman(n - 40)
+    end
+
+    def to_roman(n) when n < 90 do
+      "L" <> to_roman(n - 50)
+    end
+
+    def to_roman(n) when n < 100 do
+      "XC" <> to_roman(n - 90)
+    end
+
+    def to_roman(n) when n < 400 do
+      "C" <> to_roman(n - 100)
+    end
+
+    def to_roman(n) when n < 500 do
+      "CD" <> to_roman(n - 400)
+    end
+
+    def to_roman(n) when n < 900 do
+      "D" <> to_roman(n - 500)
+    end
+
+    def to_roman(n) when n < 1000 do
+      "CM" <> to_roman(n - 900)
+    end
+
+    def to_roman(n) do
+      "M" <> to_roman(n - 1000)
     end
 
 end
--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -1,15 +1,28 @@
 defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
-
-  testData = %{
-   1 => "I",
-   2 => "II",
-   5 => "V",
-   6 => "VI",
-   10 => "X",
-   15 => "XV",
-   20 => "XX"
+   testData = %{
+        1 => "I",
+        2 => "II",
+        4 =>"IV",
+        5 => "V",
+        6 => "VI",
+        9 =>"IX",
+        10 => "X",
+        11 =>"XI",
+        15 => "XV",
+        20 => "XX",
+        40 =>"XL",
+        49 => "XLIX",
+        50 =>"L",
+        90 => "XC",
+        100 =>"C",
+        400 =>"CD",
+        500 =>"D",
+        900 =>"CM",
+        1000 =>"M",
+        1919 => "MCMXIX",
+        2016 => "MMXVI",
    }
 
   for { arabic, roman} <- testData do
