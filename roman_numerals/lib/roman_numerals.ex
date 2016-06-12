422619dbf51fc0902a54f675ee0376dd0f96018e
Stratos Xakoustos <stratouklos@gmail.com>
2016-06-12 11:11:16 -0400

    implemented data driven tests and simple logic

--- roman_numerals/lib/roman_numerals.ex
+++ roman_numerals/lib/roman_numerals.ex
@@ -1,7 +1,15 @@
 defmodule RomanNumerals do
 
-    def to_roman(1) do
-      "I"
+    def to_roman(n) when n < 5 do
+      String.duplicate("I", n)
+    end
+
+    def to_roman(n) when n < 10 do
+      "V" <> to_roman(n - 5)
+    end
+
+    def to_roman(n)  do
+      "X" <> to_roman(n - 10)
     end
 
 end
--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -2,8 +2,22 @@ defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
 
+  testData = %{
+   1 => "I",
+   2 => "II",
+   5 => "V",
+   6 => "VI",
+   10 => "X",
+   15 => "XV",
+   20 => "XX"
+   }
 
-  test "the truth" do
-    assert RomanNumerals.to_roman(1) == "I"
+  for { arabic, roman} <- testData do
+      @arabic arabic
+      @roman roman
+      test "convert #{@arabic} to #{@roman}" do
+         assert RomanNumerals.to_roman(@arabic) == @roman
+      end
   end
+
 end
